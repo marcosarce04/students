16f1a190abb53ed444d922b54916a7ec06ee7b06
Marcos <marcosarce04@gmail.com>
2020-02-06 17:48:00 -0300

    Add all origins to use the service (Controller -> @CrossOrigin(...)) and others

--- src/main/java/com/Marcos/Controller/StudentController.java
+++ src/main/java/com/Marcos/Controller/StudentController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 @RequestMapping("/students")
 public class StudentController {
 
